4394bedc69efb6f90696b2662845e38687ce5bba
Inou Ridder <inouridder@gmail.com>
2019-04-18 15:07:18 +0200

    remove begin;rescue

--- lib/draw_static.rb
+++ lib/draw_static.rb
@@ -1,13 +1,9 @@
 require_relative 'static_routes'
 
-begin
-  ActionDispatch::Routing::Mapper.class_eval do
-    def draw_static(*controllers)
-      controllers.each do |controller|
-        StaticRoutes.for(controller, self)
-      end
+ActionDispatch::Routing::Mapper.class_eval do
+  def draw_static(*controllers)
+    controllers.each do |controller|
+      StaticRoutes.for(controller, self)
     end
   end
-rescue
-  puts "Please add this gem to rails - it is dependent on ActionDispatch::Routing::Mapper"
 end
