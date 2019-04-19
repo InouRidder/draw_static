9523fc8b2f8df291ec2aa7c900c607a411a25119
Khalil Gharbaoui <khalil@adcombi.com>
2019-04-20 01:07:19 +0200

    Use Module#prepend instead of class_eval
    There is no need for class reopening (or monkey patching) here.
    
    Prepending a module is more gracefull the method `draw_static` won't be
    hacked into ActionDispatch::Routing::Mapper anymore but simply inserted
    inside a module called DrawStatic at the very bottom of the ancestor chain.
    
    It will sit before the class itself, and Ruby will look into the module
    methods before the looking into the class. a.k.a. Inheritance over patching.
    
    Also when debugging this one can call .ancestors on it and see
    the module sitting there at the very bottom and know where this behavior
    came from.
    
    P.s:
    The tests still pass this refactor won't break the gem.✌️

--- lib/draw_static.rb
+++ lib/draw_static.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 require_relative 'static_routes'
 
-ActionDispatch::Routing::Mapper.class_eval do
+module DrawStatic
   def draw_static(*controllers)
     controllers.each do |controller|
       StaticRoutes.for(controller, self)
     end
   end
 end
+
+ActionDispatch::Routing::Mapper.prepend DrawStatic
