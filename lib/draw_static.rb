06747adb09499811e1955df0a5c80ed831332e77
Inou Ridder <inouridder@gmail.com>
2019-11-20 15:22:08 +0100

    FEAT: fetch the prefixed modules from a child route;

--- lib/draw_static.rb
+++ lib/draw_static.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
+require 'pry'
 
 require_relative 'static_routes'
 
 module DrawStatic
   def draw_static(*arguments)
     generator = StaticRoutes.new(context: self)
-    generator.for(arguments)
+
+    generator.for(arguments: arguments, prefixed_modules: get_prefixed_modules)
+  end
+
+  def get_prefixed_modules
+    instance_variable_get('@scope')&.instance_variable_get('@hash')&.try(:[], :module)
   end
 end
 
